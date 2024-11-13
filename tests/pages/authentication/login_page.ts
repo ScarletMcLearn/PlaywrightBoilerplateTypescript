93ccc3f308a78b32e84afd83e29fe593cbee6505
Scarlet McLearn <ratul_shams@yahoo.com>
2024-11-13 20:26:25 +0600

    Locators segrate
    Locators segrate

--- tests/pages/authentication/login_page.ts
+++ tests/pages/authentication/login_page.ts
@@ -4,29 +4,30 @@ import { BasePage } from '../base/base_page';
 import { Page } from '@playwright/test';
 
 export class LoginPage extends BasePage {
-  private usernameField = this.page.getByLabel('Username');
-  private passwordField = this.page.getByLabel('Password');
-  private submitButton = this.page.getByRole('button', { name: 'Submit' });
-  private successMessageSelector =
-    'role=heading[name="Logged In Successfully"]';
+  private usernameField: Locator;
+  private passwordField: Locator;
+  private submitButton: Locator;
+  private successMessage: Locator;
 
   constructor(page: Page) {
     super(page);
+    this.usernameField = page.locator('input[name="username"]');
+    this.passwordField = page.locator('input[name="password"]');
+    this.submitButton = page.getByRole('button', { name: 'Submit' });
+    this.successMessage = page.getByRole('heading', {
+      name: 'Logged In Successfully',
+    });
   }
 
   async login(username: string, password: string): Promise<void> {
     await this.usernameField.fill(username);
     await this.passwordField.fill(password);
+    await this.submitButton.scrollIntoViewIfNeeded();
     await this.submitButton.click();
   }
 
   async isLoginSuccessful(): Promise<boolean> {
-    const successMessage = await this.page.isVisible(
-      this.successMessageSelector,
-    );
-    console.log('###############2');
-    console.log(successMessage);
-    console.log('!!!!!!!!!!!!!!!!');
-    return successMessage;
+    await this.successMessage.waitFor({ timeout: 5000 });
+    return await this.successMessage.isVisible();
   }
 }
