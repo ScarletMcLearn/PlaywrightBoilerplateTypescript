e4546f7889203f4e2df84fe79d10e1074a575a24
Scarlet McLearn <ratul_shams@yahoo.com>
2024-11-15 16:25:29 +0600

    seperated browsers
    seperated browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
-
+import { browsers } from './tests/utils/browsers/browser_config';
 // dotenv.config();
 
 dotenv.config({ path: path.resolve(__dirname, 'tests/utils/env/.env') });
@@ -42,42 +42,7 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+  projects: browsers,
 
   /* Run your local dev server before starting the tests */
   // webServer: {
